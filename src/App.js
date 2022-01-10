218796203784511fa0478f552022630405dd297f
katerynabugaieva <sonnensmile@gmail.com>
2022-01-10 22:32:10 +0100

    add fans

--- src/App.js
+++ src/App.js
@@ -14,21 +14,36 @@ function App() {
         </div>
         <div class="column">
           <img src="fan5.png" alt="fans" />
-          <img src="fan8.png" alt="fans" />
+          <img src="fan6.png" alt="fans" />
         </div>
       </div>
       <div class="row">
         <div class="column">
-          <img src="fan5.png" alt="fans" />
+          <img src="fan7.png" alt="fans" />
           <img src="fan8.png" alt="fans" />
         </div>
         <div class="column">
-          <img src="fan6.png" alt="fans" />
           <img src="fan9.png" alt="fans" />
+          <img src="fan10.png" alt="fans" />
         </div>
         <div class="column">
-          <img src="fan6.png" alt="fans" />
-          <img src="fan9.png" alt="fans" />
+          <img src="fan11.png" alt="fans" />
+          <img src="fan12.png" alt="fans" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <img src="fan13.png" alt="fans" />
+          <img src="fan14.png" alt="fans" />
+        </div>
+        <div class="column">
+          <img src="fan15.png" alt="fans" />
+          <img src="fan16.png" alt="fans" />
+        </div>
+        <div class="column">
+          <img src="fan17.png" alt="fans" />
+          <img src="fan18.png" alt="fans" />
         </div>
       </div>
     </div>
